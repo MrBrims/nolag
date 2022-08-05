78eb8ca30a215357184067a49b70a10e9842a36f
MrBrims <magistrbrims@gmail.com>
2022-08-05 21:46:28 +0800

    added related-news slider and remove color

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+  let relatedNewsImg = new Swiper(".related-news__slider", {
+    slidesPerView: 4,
+    slidesPerGroup: 1,
+    spaceBetween: 15,
+    loop: true,
+    navigation: {
+      nextEl: ".related-news__slider-next",
+      prevEl: ".related-news__slider-prev",
+    },
+    pagination: {
+      el: ".related-news__slider-pagination",
+      clickable: true,
+    },
+  });
 
 })
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ function styles() {
 
 function scripts() {
   return src([
-    
+    'node_modules/swiper/swiper-bundle.min.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
